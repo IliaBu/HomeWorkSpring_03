581fd351ee56583d5a9009838da05fb26dcfe7d2
IliaBu <bunamaksi@yandex.ru>
2024-04-06 19:28:11 +0500

    Create new project

---
+++ src/main/java/ru/gb_spring/homeworkspring_03/HomeWorkSpring03Application.java
@@ -0,0 +1,13 @@
+package ru.gb_spring.homeworkspring_03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWorkSpring03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWorkSpring03Application.class, args);
+    }
+
+}
